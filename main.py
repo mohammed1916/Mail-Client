bb86552a2bb7a9b1aec119c27bcfc90f326980d8
Mohammed <arrahmaan9901@gmail.com>
2023-05-05 17:41:01 +0530

    fetch the data with query param and store in db

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+from googleapiclient.discovery import build
 from Functions.Auth.auth import Authenticate
 from Functions.Search.search import Search
+
+# Define rule-based operations function
+
+
+def process_emails(service):
+    # Define the search query for the emails you want to process
+    query = 'is:unread '
+
+    # Search for emails matching the query
+    response = service.users().messages().list(userId='me', q=query).execute()
+
+    # Process each email
+    if 'messages' in response:
+        for message in response['messages']:
+            msg = service.users().messages().get(
+                userId='me', id=message['id']).execute()
+            print(msg)
+
+
 if __name__ == '__main__':
     # get the Gmail API service
-    service = Authenticate.gmail_authenticate()
-    print(Search.search_messages(service=service, query="GUVI"))
+    try:
+        service = Authenticate.gmail_authenticate()
+    except Exception:
+        print(f'An error occurred: {Exception}')
+    print(Search.search_messages(service=service, query="is:unread"))
+    # process_emails(service)
